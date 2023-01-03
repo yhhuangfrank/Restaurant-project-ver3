284cb9c1f3e754fef1137a2d32ade03a2321a311
frank <frankcool321@gmail.com>
2023-01-03 22:10:03 +0800

    refactor: modify register function and ui

--- routes/modules/auth.js
+++ routes/modules/auth.js
@@ -9,34 +9,47 @@ router.get("/register", (req, res) => {
   return res.render("register");
 });
 
-router.post("/register", async (req, res) => {
-  const { name, username, password } = req.body;
+router.post("/register", (req, res) => {
+  const { name, username, password, confirmPassword } = req.body;
+  let register_fail_msg = [];
   if (password.length < 8) {
-    //* 更新flash 的error_msg
-    req.flash("fail_msg", "密碼長度過短，至少需要8位字元");
-    console.log("提交表單");
-    return res.render("register", { name, username, password });
+    register_fail_msg.push({ message: "密碼過短，至少需8位" });
   }
-  try {
-    //- 若輸入內容通過->檢查是否註冊過
-    const foundUser = await User.findOne({ email: username });
-    if (foundUser) {
-      req.flash("fail_msg", "此信箱已被註冊過囉!");
-      return res.redirect("/auth/register");
-    }
-    //- 若為新用戶(進行密碼雜湊與存入資料庫)
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({
+  if (password !== confirmPassword) {
+    register_fail_msg.push({ message: "密碼與確認密碼不符!" });
+  }
+  if (register_fail_msg.length) {
+    return res.render("register", {
       name,
-      email: username,
-      password: hashedPassword,
+      username,
+      password,
+      confirmPassword,
+      register_fail_msg,
     });
-    await newUser.save();
-    req.flash(req.flash("success_msg", "註冊成功!現在可以登入系統了!"));
-    return res.redirect("/home");
-  } catch (err) {
-    console.log(err);
   }
+  //- 若輸入內容通過->檢查是否註冊過
+  return User.findOne({ email: username })
+    .then((user) => {
+      if (user) {
+        req.flash("fail_msg", "此信箱已被註冊過囉!");
+        return res.redirect("/auth/register");
+      }
+      //- 若為新用戶(進行密碼雜湊與存入資料庫)
+      return bcrypt
+        .hash(password, 10)
+        .then((hashedPassword) =>
+          User.create({
+            name,
+            email: username,
+            password: hashedPassword,
+          })
+        )
+        .then(() => {
+          req.flash(req.flash("success_msg", "註冊成功!現在可以登入系統了!"));
+          return res.redirect("/home");
+        });
+    })
+    .catch((err) => console.log(err));
 });
 
 //- 驗證本地會員登入(使用passport提供middleware)
